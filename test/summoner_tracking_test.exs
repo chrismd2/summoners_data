7170d72da38a3ec79174645501fc44dff903f630
chris christenson <charlie.mike.1991@gmail.com>
2024-02-19 13:59:32 -0500

    added a sleep to wait for the tracker to update and a non blocking means to test scheduled update check

--- test/summoner_tracking_test.exs
+++ test/summoner_tracking_test.exs
@@ -1,13 +1,18 @@
 defmodule SummonerTrackingTest do
   use ExUnit.Case
   alias Summoners.SummonerTracking
+  alias Task.Supervisor
+  alias Summoners.TaskSupervisor
 
   describe "summoner tracking testing" do
     test "summoner play data returns a match id and saves it in recent games for tracking" do
-      match_ids = SummonerTracking.scheduled_update_check([summoner_monitor_time: 1])
-      assert Enum.sort(match_ids) == SummonerTracking.monitored_summoners()
-      |> Enum.map(&Map.get(&1, :recent_match_id))
-      |> Enum.sort()
+      Supervisor.start_child(TaskSupervisor, &scheduled_update_check_test/0)
+      :timer.sleep(500)
+      assert SummonerTracking.monitored_summoners()
+      |> Enum.all?(& !is_nil(&1.recent_match_id))
     end
+  end
+  defp scheduled_update_check_test() do
+    SummonerTracking.scheduled_update_check([summoner_monitor_time: 1])
   end
 end
